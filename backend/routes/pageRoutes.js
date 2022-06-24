6c541fd8e5037ec56a247dd7ac9b75811c9ca5ba
David <Dcarlon00@yahoo.com>
2022-06-24 13:04:01 -0500

    added more on routing

--- backend/routes/pageRoutes.js
+++ backend/routes/pageRoutes.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { getUsers, setUsers, updateUsers, deleteUsers} = require('../controllers/userController')
+
+router.route('/').get(getUsers).post(setUsers)
+router.route('/:id').delete(deleteUsers).put(updateUsers)
+
+router.get('/', (getUsers))
+
+router.post('/', (setUsers))
+
+router.put('/:id', (updateUsers))
+
+router.delete('/:id', (deleteUsers))
 
 module.exports = router
-
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get User' })
-})
-
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set User' })
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update user ${req.params.id}` })
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Delete user ${req.params.id}` })
-})
