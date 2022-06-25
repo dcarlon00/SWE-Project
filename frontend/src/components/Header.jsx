aee680cfcb26b9febc979aae795b49c554275250
David <Dcarlon00@yahoo.com>
2022-06-25 02:50:37 -0500

    Login and Register raw frontend done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
+/* import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice' */
+
 function Header() {
+/*     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth) */
+
+
   return (
     <header className='header'>
         <div className="logo">
