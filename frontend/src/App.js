d8e4f958fb3d4b6edf300a17fde6706263e62f5e
David <Dcarlon00@yahoo.com>
2022-06-25 13:41:58 -0500

    Frontend 3/4 pages done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Outlet} from 'react-router-dom'
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+/* import { Button } from '@themesberg/react-bootstrap' */
+
+
+
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+/*     const navigate = useNavigate()
+
+    const {user} = useSelector((state) => state.auth) */
+
+ /*    useEffect(() => {
+        if(!user){
+            navigate('/login') //should be /login
+        }
+    }, [user, navigate]) */
+
+    return <>
+    
+      <header className='header'>
+        <div className="logo">
+            <Link to='/'>Fuel Application</Link>
+        </div>
+        <ul>
+            <li>
+                <div class="btn-group">
+                    <button className="btn">
+                        <FaSignOutAlt/> Logout
+                    </button>
+                </div>
+            </li>  
+        </ul>
+    </header>
+    <div>
+        
+        <header className="container2">
+            <h1>Welcome 'User' to your Dashboard</h1>
+            <p>Below are your user functions.</p>
+        </header>
+        
+    </div>
+    <header className='container'>
+        <div class="btn-group" style={{width:`100%`}}>
+            <button style={{width:`33.3%`}}>Create Fuel Quote</button>
+            <button style={{width:`33.3%`}}>View Fuel Quote History</button>
+            <button style={{width:`33.3%`}}>Function 3</button>
+        </div>
+    </header>
+
+
+    </>
+    
+
+
+/*     return <>
+        <section className="heading">
+            <h1>Welcome 'user's name'</h1>
+        </section>
+    
+    </> */
+    
 }
 
 export default Dashboard
