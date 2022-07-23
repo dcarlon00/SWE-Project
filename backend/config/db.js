cf43b956ad5456b3bbab274af88b77aa84bad366
gjgiron <pivuzix@gmail.com>
2022-07-23 12:56:05 -0500

    skip

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb+srv://team7group:cosc4353@cluster0.8kbwb.mongodb.net/?retryWrites=true&w=majority")
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.
         underline);
