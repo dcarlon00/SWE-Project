62696d180894967ae55fd64e2938b1cfae99d231
WilliamStrong95 <99148305+WilliamStrong95@users.noreply.github.com>
2022-07-23 20:42:40 -0500

    Create fuelForm.js

---
+++ modules/fuelForm.js
@@ -0,0 +1,10 @@
+function fuelForm(gallons, address, deliveryDate ,pricePerGallon) 
+{
+    var total = pricePerGallon * gallons;
+    if (arguments.length > 0)
+    {
+        return [gallons, address, deliveryDate, pricePerGallon, total];
+    }
+    // Add forwarding for database on the next assignment
+}
+module.exports = fuelForm;
