f7dfffe162030262db1f57b399193a67e81ef528
WilliamStrong95 <99148305+WilliamStrong95@users.noreply.github.com>
2022-07-23 20:43:02 -0500

    Add files via upload

---
+++ modules/pricingModule.js
@@ -0,0 +1,13 @@
+function pricingModule(gallons, pricePerGallon) 
+{
+    // Add forwarding for frontend on the next assignment
+}
+
+class pricing {
+    constructor(gallon, price) 
+    {
+
+    }
+}
+
+module.exports = fuelForm;
