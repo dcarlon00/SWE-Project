2aa0dd44f9d49e1d99ad4b760e9848241b985420
WilliamStrong95 <99148305+WilliamStrong95@users.noreply.github.com>
2022-07-24 20:32:36 -0500

    Update ProfileFinish.jsx

--- frontend/src/pages/ProfileFinish.jsx
+++ frontend/src/pages/ProfileFinish.jsx
@@ -1,37 +1,63 @@
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
-import $ from 'jquery';
+import {useDispatch, useSelector} from 'react-redux'
 import ModifyProfileform from '../components/ModifyProfileForm'
+import Spinner from '../components/Spinner'
+import {getProfile} from '../features/profile/profileSlice'
+
 /* import { Button } from '@themesberg/react-bootstrap' */
 
 function ProfileFinish() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-<div id="idofthedivtohide">
-  <span>Success!</span>
-</div>
 
-/*     const navigate = useNavigate()
+    const {user} = useSelector((state) => state.auth) 
 
-    const {user} = useSelector((state) => state.auth) */
-
- /*    useEffect(() => {
+    useEffect(() => {
         if(!user){
-            navigate('/login') //should be /login
+            navigate('/Login')
         }
-    }, [user, navigate]) */
 
+    }, [user, navigate])
+
+    const {profile, isLoading, isError, message} = useSelector((state) => state.profile)
+
+    useEffect(() => {
+        if(isError){
+            console.log(message);
+        }
+
+        dispatch(getProfile())
+
+    }, [user, navigate, isError, message, dispatch])
+
+    if(isLoading){
+        return <Spinner />
+    }
+    
+    const profileData = {
+        userName: `${profile[0].name}`,
+        addressOne: `${profile[0].addressOne}`,
+        addressTwo: `${profile[0].addressTwo}`,
+        city: `${profile[0].city}`,
+        state: `${profile[0].state}`,
+        zipcode: `${profile[0].zipcode}`,
+        id: `${profile[0]._id}`,
+    }
+    
     return  (
     <>
-        <ModifyProfileform />
+        <ModifyProfileform 
+            userName={profileData.userName} 
+            addressOne={profileData.addressOne} 
+            addressTwo={profileData.addressTwo} 
+            city={profileData.city} 
+            state={profileData.state} 
+            zipcode={profileData.zipcode} 
+            id={profileData.id}/>
     </>
     )
-
-
- 
-    // The above script only works when ?newuser is typed manually, not from the register page.
 }
 
 export default ProfileFinish
