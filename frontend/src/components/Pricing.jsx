1d7cf2419aae85f6562436f4ce7c4d9e39a59961
WilliamStrong95 <99148305+WilliamStrong95@users.noreply.github.com>
2022-08-02 17:43:38 -0500

    Create Pricing.jsx

---
+++ frontend/src/components/Pricing.jsx
@@ -0,0 +1,44 @@
+function Pricing(numGallons, delState, formLength) {
+    var gallons = parseInt(numGallons)
+    var locationFactor
+    var rateHistoryFactor
+    var galReqFactor
+    var companyProfitFactor = .1 
+    var margin   
+    var ppGal
+
+    console.log(formLength)
+
+    if (delState == "TX") {
+        locationFactor = .02
+    } else {
+        locationFactor = .04
+    }
+
+    if (formLength > 0) {
+        rateHistoryFactor = .01
+    } else {
+        rateHistoryFactor = 0
+    }
+
+    if (gallons > 1000) {
+        galReqFactor = .02
+    } else {
+        galReqFactor = .03
+    }
+
+    margin = (locationFactor - rateHistoryFactor + galReqFactor + companyProfitFactor) * 1.50
+    ppGal = margin + 1.50
+    
+    var total = gallons * ppGal
+    var roundedTotal = total.toFixed(2)
+
+    return (
+        {
+            ppGal,
+            roundedTotal,
+        }
+    )
+}
+
+export default Pricing
