4bfc55bc3302f32912ae9e5f8979a7da9f0a4b14
David <Dcarlon00@yahoo.com>
2022-08-02 16:41:57 -0500

    Try fix to DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,28 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.
+        underline);
+    }catch (error){
+        console.log(error);
+        process.exit(1)
+
+    }
+}
+
+module.exports = connectDB */
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI,
+    {
+        useNewUrlParser: "true"
+    })
+
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.
         underline);
