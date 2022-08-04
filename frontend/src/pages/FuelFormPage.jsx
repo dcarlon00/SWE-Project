47046bc65af773f778706d08ef4401c7ff2e6289
WilliamStrong95 <99148305+WilliamStrong95@users.noreply.github.com>
2022-08-03 19:08:09 -0500

    Update page missed in previous merge

--- frontend/src/pages/FuelFormPage.jsx
+++ frontend/src/pages/FuelFormPage.jsx
@@ -1,7 +1,41 @@
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
 import FuelForm from '../components/FuelForm'
+import Spinner from '../components/Spinner'
+import {getForms, reset} from '../features/forms/formSlice'
 
 function FuelFormPage() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user} = useSelector((state) => state.auth)
+    const {forms, isLoading, isError, message} = useSelector((state) => state.forms)
+
+    useEffect(() => {
+        if(isError){
+            console.log(message);
+        }
+
+
+        if(!user) {
+            navigate('/login')
+        }
+
+        dispatch(getForms())
+
+        return () =>{
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch]);
+
+    const formsLength = forms.length
+
+    if(isLoading){
+        return <Spinner />
+    }
+
     return <>
     <header className='header'>
         <div className="logo">
@@ -26,7 +60,7 @@ function FuelFormPage() {
         <p>Please fill in the fields below</p>
     </section> 
 
-   <FuelForm /> {/* Fuel Form component */}
+   <FuelForm formLength={formsLength}/> {/* Fuel Form component */}
     </>
 }
 
